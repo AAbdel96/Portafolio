d41329e0b2c815e5119937a28f914fe8dc01a641
Ali <Ali@DESKTOP-JJA9723>
2022-07-18 19:31:06 +0200

    Add project files.

---
+++ Portafolio/Program.cs
@@ -0,0 +1,42 @@
+using Portafolio.Servicios;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+//Inyeccion de dependencias
+builder.Services.AddTransient<IRepositorioProyectos,RepositorioProyectos>();
+
+
+
+//pruebas
+
+
+builder.Services.AddTransient<ServicioTransitorio>();
+builder.Services.AddScoped<ServicioDelimitado>();
+builder.Services.AddSingleton<ServicioUnico>();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,49 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+    public class RepositorioProyectos : IRepositorioProyectos
+    {
+
+
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>() {
+                new Proyecto
+                {
+                    Titulo = "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP.NET Core",
+                    Url = "https://amazon.com",
+                    ImageURL = "/images/amazon.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "New York Times",
+                    Descripcion = "Página de noticias en React",
+                    Url = "https://nytimes.com",
+                    ImageURL = "/images/nyt.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "Reddit",
+                    Descripcion = "Red social para compartir en comunidades",
+                    Url = "https://reddit.com",
+                    ImageURL = "/images/reddit.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "Steam",
+                    Descripcion = "Tienda en línea para comprar videojuegos",
+                    Url = "https://store.steampowered.com",
+                    ImageURL = "/images/steam.png"
+                },
+
+            };
+        }
+    }
+}
